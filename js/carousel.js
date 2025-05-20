9223dd97bde5fadeb4f565f8bd33e9b54698f115
Abdellah <abdellah.ait59@gmail.com>
2025-05-20 20:03:54 +0200

    feat(carousel.js): Implement showSlides function for improved slide navigation

--- js/carousel.js
+++ js/carousel.js
@@ -8,3 +8,29 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+function showSlides(n) {
+  const imagesContainer = document.querySelector(".carousel .images"); 
+  if (!imagesContainer){
+    console.error("Le conteneur '.carousel .images' not found.");
+    return;
+  }
+
+  const slides = imagesContainer.querySelectorAll(".image"); 
+  const totalSlides = slides.length;
+
+ 
+  if (n > totalSlides){
+    slideIndex = 1;
+  }
+  if (n < 1){
+
+    slideIndex = totalSlides;
+  }
+
+
+  for (let i = 0; i < totalSlides; i++){
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
